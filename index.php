f197957e05ec85513161098e878ad1441ae7d874
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2022-11-14 20:25:10 -0300

    Restructuring of the main page for demonstration

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
-$site_json = file_get_contents("site.json");
-$content = json_decode($site_json, true);
-
-$lang = "en";
-if (isset($_GET["lang"]) && $_GET["lang"] == "pt") {
-  $lang = "pt";
+$lang = "en-us";
+if (isset($_GET["lang"]) && $_GET["lang"] == "pt-br") {
+  $lang = "pt-br";
 }
 
+$file_content = file_get_contents("assets/content/{$lang}.json");
+$content = json_decode($file_content, true);
 ?>
+
 <!DOCTYPE html>
-<html lang="<?php echo $lang ?>">
+<html lang="<?php echo $content["language"] ?>">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>OLIFX</title>
+  <title><?php echo $content["element"]["title"] ?></title>
 </head>
 <body>
-  <h1><?php echo $content['title'][$lang] ?></h1>
+  <h1><?php echo $content["element"]["title"] ?></h1>
 
-  <p><?php echo $content['paragraph'][$lang] ?></p>
+  <p><?php echo $content["element"]["text"] ?></p>
 
-  <a href="index.php?lang=pt">Português</a>
+  <a href="index.php?lang=pt-br">Português</a>
   <br>
   <a href="index.php">English</a>
 </body>
